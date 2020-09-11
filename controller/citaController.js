fe10ea3ded5ceea59bf5c45e7f70681d28a7f252
miguel <miguel.ramirez7@unmsm.edu.pe>
2020-09-11 12:51:07 -0500

    con BD prueba

--- controller/citaController.js
+++ controller/citaController.js
@@ -43,6 +43,7 @@ exports.GenerarNuevaCita = async function (req, res) {
                           hora_inicio: req.body.hora_inicio,
                           hora_fin: req.body.hora_fin,
                           doctor: doctor,
+                          
                         });
                         //si horario exisete
                         if (horario) {
@@ -215,19 +216,16 @@ exports.Actualizar_Citas = async function (req, res) {
             const paciente = await User.findOne({ _id: req.params.id });
             const pacientes_cita_tmp = paciente.cita;
             const find_index = pacientes_cita_tmp.indexOf(req.body.id_cita);
+
             //sacamos la cita del paciente
             pacientes_cita_tmp.splice(find_index, 1);
-
+            
             //sacamos la cita del doctor
             const doctor = await Doctor.findOne({ _id: req.body.id_doctor });
             const doctor_cita_tmp = doctor.cita;
             const find_index_doctor = doctor_cita_tmp.indexOf(req.body.id_cita);
             doctor_cita_tmp.splice(find_index_doctor, 1);
 
-            await doctor.updateOne(
-              { _id: req.body.id_doctor },
-              { $set: { cita: doctor_cita_tmp } }
-            );
 
             paciente.save()
             doctor.save()
@@ -382,7 +380,7 @@ exports.Eliminar_cita = async function (req, res) {
             await cita.remove();
             paciente.save()
             doctor.save()
-            
+
             res.json({ msg: "cita eliminada" ,horario:horario,doctor:doctor,paciente:paciente});
           }
         });
