fce1179d546b1b6a44099c65d1aeb8c934657b1e
unknown <richard896qa@gmail.com>
2020-09-13 20:05:24 -0500

    Agregando devolver correo

--- models/user.js
+++ models/user.js
@@ -81,7 +81,6 @@ UserSchema.methods.toJSON=function(){
     let user= this;
     let userObject = user.toObject();
     delete userObject.password;
-    delete userObject.email;
     return userObject;
 };
 
